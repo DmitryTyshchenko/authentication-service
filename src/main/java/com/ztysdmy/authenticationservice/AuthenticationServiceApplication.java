ff1ee8d5d17d41ea593eb76ed75d30b58d45f89b
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-08-23 16:14:27 +0300

    some changes

--- src/main/java/com/ztysdmy/authenticationservice/AuthenticationServiceApplication.java
+++ src/main/java/com/ztysdmy/authenticationservice/AuthenticationServiceApplication.java
@@ -2,8 +2,16 @@ package com.ztysdmy.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+/**
+ * https://stackoverflow.com/questions/49601371/spring-boot-oauth2-full-authentication-is-required-to-access-this-resource
+ * @author dmytro.tyshchenko
+ *
+ */
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
