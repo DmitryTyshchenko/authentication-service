cb1d95aa693b39a35070a3e0eea98fe9dceaa662
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-08-29 10:43:21 +0300

    create Authentication class

--- src/it/java/com/ztysdmy/authenticationservice/AuthTest.java
+++ src/it/java/com/ztysdmy/authenticationservice/AuthTest.java
@@ -1,5 +1,6 @@
 package com.ztysdmy.authenticationservice;
 
+import java.io.ByteArrayInputStream;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URI;
@@ -8,10 +9,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpResponse.BodySubscribers;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 
 import org.junit.Test;
 
@@ -68,4 +74,29 @@ public class AuthTest {
 		return BodyPublishers.ofString(builder.toString());
 	}
 	
+	public static class JsonBodyHandler<T> implements HttpResponse.BodyHandler<T> {
+		  private final Jsonb jsonb;
+		  private final Class<T> type;
+
+		  public static <T> JsonBodyHandler<T> jsonBodyHandler(final Class<T> type) {
+		    return jsonBodyHandler(JsonbBuilder.create(), type);
+		  }
+
+		  public static <T> JsonBodyHandler<T> jsonBodyHandler(final Jsonb jsonb,
+		      final Class<T> type) {
+		    return new JsonBodyHandler<>(jsonb, type);
+		  }
+
+		  private JsonBodyHandler(Jsonb jsonb, Class<T> type) {
+		    this.jsonb = jsonb;
+		    this.type = type;
+		  }
+
+		  @Override
+		  public HttpResponse.BodySubscriber<T> apply(
+		      final HttpResponse.ResponseInfo responseInfo) {
+		    return BodySubscribers.mapping(BodySubscribers.ofByteArray(),
+		        byteArray -> this.jsonb.fromJson(new ByteArrayInputStream(byteArray), this.type));
+		  }
+		}
 }
---
+++ src/test/java/com/ztysdmy/authenticationservice/model/AuthenticationTest.java
@@ -0,0 +1,20 @@
+package com.ztysdmy.authenticationservice.model;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AuthenticationTest {
+
+	private String OathResponse  = "{\"access_token\":\"43a999cf-45d8-4cc3-8442-c903426940f9\",\"token_type\":\"bearer\",\"refresh_token\":\"69904df5-7208-432f-8f6d-c755eeda6bdb\",\"expires_in\":43199,\"scope\":\"webclient mobileclient\"}";
+	
+	@Test
+	public void shouldCreateAuthenticationFromOAthResponse() throws Exception {
+		Jsonb jsonb = JsonbBuilder.create();
+		Authentication authentication = jsonb.fromJson(OathResponse, Authentication.class);
+		Assert.assertNotNull(authentication);
+		Assert.assertEquals("43a999cf-45d8-4cc3-8442-c903426940f9", authentication.getAccess_token());
+	}
+}
